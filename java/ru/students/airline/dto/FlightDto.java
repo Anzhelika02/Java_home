92de7e8f78ca686e2da71ec51610e73f03090545
Latokha-Sofiia <132939164+Latokha-Sofiia@users.noreply.github.com>
2023-12-22 22:32:49 +0700

    Update FlightDto.java

--- java/ru/students/airline/dto/FlightDto.java
+++ java/ru/students/airline/dto/FlightDto.java
@@ -1,1 +1,29 @@
+package ru.students.airline.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Data // Lombok аннотация для автоматического создания геттеров, сеттеров, equals, hashCode и toString
+@NoArgsConstructor // Lombok аннотация для создания конструктора без аргументов
+@AllArgsConstructor // Lombok аннотация для создания конструктора со всеми аргументами
+public class FlightDto {
+
+    String number; // Уникальный номер рейса
+
+    private String departureCity; // Город отправления
+
+    private String arrivalCity; // Город прибытия
+
+    private LocalTime departureTime; // Время отправления
+
+    private LocalTime arrivalTime; // Время прибытия
+
+    private Integer cost; // Стоимость билета
+
+    private Integer seats; // Количество мест в самолете
+
+    private String weekdays; // Дни недели, в которые осуществляется рейс
+}
