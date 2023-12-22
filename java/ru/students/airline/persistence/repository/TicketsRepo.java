c652675e8679581146dea9c1c9ad9acd7de03465
Daria28A <92773554+Daria28A@users.noreply.github.com>
2023-12-22 23:09:59 +0700

    Update TicketsRepo.java

--- java/ru/students/airline/persistence/repository/TicketsRepo.java
+++ java/ru/students/airline/persistence/repository/TicketsRepo.java
@@ -1,13 +1,17 @@
-package ru.students.airline.persistence.repository;
+package ru.students.airline.persistence.repository; //Указываем на принадлежность данного файла к определенному пакету в Java. 
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import ru.students.airline.persistence.entity.Ticket;
+import org.springframework.data.jpa.repository.Query; //Директива import, которая позволяет использовать аннотацию `@Query` из пакета `org.springframework.data.jpa.repository`. Используется для определения пользовательских SQL-запросов в репозитории.
+import org.springframework.data.repository.CrudRepository; //Импортируем интерфейс `CrudRepository` из пакета `org.springframework.data.repository`. `CrudRepository` предоставляет базовые методы для работы с сущностями, включая сохранение, удаление и поиск.
 
-import java.util.Optional;
+import ru.students.airline.persistence.entity.Ticket;//Этот оператор импортирует класс `Ticket` из пакета `ru.students.airline.persistence.entity` в текущий файл. Ticket` представляет конкретный билет.
 
-public interface TicketsRepo extends CrudRepository<Ticket, Integer> {
+
+import java.util.Optional; //Импортируем класс `Optional` из пакета `java.util`. `Optional` представляет контейнер, который может содержать или отсутствовать значение  и используется для представления результатов операций, которые могут возвращать какое-то значение или ничего.
+
+public interface TicketsRepo extends CrudRepository<Ticket, Integer> { //Объявляем интерфейс с именем `TicketsRepo`, который расширяет (extends) интерфейс `CrudRepository` для работы с сущностью типа `Ticket` с ID типа `Integer`.
     @Query(value = "INSERT INTO tickets  (flight_date_id, username, reserved_seats)" +
-            " VALUES (?1, ?2, ?3)", nativeQuery = true)
-    Optional<Ticket> saveTicket(Integer flightDateId, String username, Integer reservedSeats);
+            " VALUES (?1, ?2, ?3)", nativeQuery = true) //Используем аннотацию `@Query`, которая позволяет определить SQL-запрос, который используется для вставки записи в таблицу "tickets" с указанными значениями.
+
+    Optional<Ticket> saveTicket(Integer flightDateId, String username, Integer reservedSeats); //Этот метод объявляет операцию для сохранения нового билета. Метод возвращает `Optional`, что указывает на то, что результат операции может отсутствовать (например, если сохранение завершилось ошибкой).
+
 }
